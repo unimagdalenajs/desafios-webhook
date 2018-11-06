4a7aa6ca729f22bae8d28fad9d6e6b43751dd646
Jean Benitez <jkbdiaz@gmail.com>
2018-11-06 00:57:02 -0500

    fix comment author issue assigned message

--- src/handler.js
+++ src/handler.js
@@ -105,7 +105,7 @@ module.exports = function (req, res, errorCallback) {
           owner: repoOwner,
           number: issueNumber,
           body: isAssignedToYou
-            ? `El desafio ya está asignado a ti. Ya puedes empezar a resolverlo!`
+            ? `El desafio ya habia sido asignado a ti anteriormente. Ya deberias estar resolviéndolo! Animo!`
             : `Lo siento @${commentAuthor}, este desafío está asignado a alguien mas.`,
         });
   
