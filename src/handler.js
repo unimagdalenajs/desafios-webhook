295abe9fcea123f888e7875f94526ad5dd0d8e53
Jean Benitez <jkbdiaz@gmail.com>
2018-11-05 22:29:27 -0500

    Automate merge using webhook

--- src/handler.js
+++ src/handler.js
@@ -36,6 +36,44 @@ module.exports = function (req, res, errorCallback) {
     const repoOwner = payload.repository.owner.login;
     const commentAuthor = payload.comment.user.login;
     const commentBody = payload.comment.body;
+    const pull_request = payload.issue.pull_request;
+
+    if (pull_request) {
+      if (ommentBody.includes('/merge')) {
+        const issueIds = commentBody.match(/#\d+/g) || [];
+        if (Array.isArray(issueIds) && issueIds.length === 2) {
+          const [fixed, created] = issueIds;
+
+          await Promise.all([
+            // close fixed issue
+            octokit.issues.edit({
+              repo: repoName,
+              owner: repoOwner,
+              number: fixed,
+              state: 'closed',
+              labels: [],
+            }),
+            // make created issue as available
+            octokit.issues.replaceAllLabels({
+              repo: repoName,
+              owner: repoOwner,
+              number: created,
+              labels: ["disponible"],
+            }),
+            // A comment in the PR
+            octokit.issues.createComment({
+              repo: repoName,
+              owner: repoOwner,
+              number: issueNumber,
+              body: `Genial @${commentAuthor}! Acabas de superar el desafio.
+              Espero hayas aprendido algo valioso y que hayas disfrutado hacer el reto, recuerda que la práctica hace al Maestro.
+              Felicidades!!!`,
+            }),
+          ]);
+        }
+      }
+      return respond("No hay nada que hacer aqui.");
+    }
 
     // If it's not a /request comment
     if (!commentBody.includes('/request')) {
@@ -70,55 +108,7 @@ module.exports = function (req, res, errorCallback) {
         body: `Hey @${commentAuthor}, el desafio es todo tuyo.`,
       }),
     ]);
-    
   });
-
-  // handler.on('issue_comment', async function (event) {
-
-  //   // Extract a few useful things from the payload
-  //   const { payload } = event;
-  //   const labels = payload.issue.labels;
-  //   const issueNumber = payload.issue.number;
-  //   const repoName = payload.repository.name;
-  //   const repoOwner = payload.repository.owner.login;
-  //   const commentAuthor = payload.comment.user.login;
-  //   const commentBody = payload.comment.body;
-
-  //   // If it's not a /request comment
-  //   if (!commentBody.includes('/request')) {
-  //     return respond("No hay nada que hacer aqui.");
-  //   }
-
-  //   // If it's already assigned, inform the user and do nothing.
-  //   const isAssigned = labels.find(({ name }) => name === "asignado");
-  //   if (isAssigned) {
-  //     await octokit.issues.createComment({
-  //       repo: repoName,
-  //       owner: repoOwner,
-  //       number: issueNumber,
-  //       body: `Lo siento @${commentAuthor}, este desafío está asignado a alguien mas.`,
-  //     });
-
-  //     return respond("El desafio ya tiene a alguien asignado.");
-  //   }
-
-  //   // Else apply new labels and inform the user
-  //   await Promise.all([
-  //     octokit.issues.replaceAllLabels({
-  //       repo: repoName,
-  //       owner: repoOwner,
-  //       number: issueNumber,
-  //       labels: ["asignado", `asignado:${commentAuthor}`],
-  //     }),
-  //     octokit.issues.createComment({
-  //       repo: repoName,
-  //       owner: repoOwner,
-  //       number: issueNumber,
-  //       body: `Hey @${commentAuthor}, el desafio es todo tuyo.`,
-  //     }),
-  //   ]);
-    
-  // });
 
   handler(req, res, errorCallback);
 
